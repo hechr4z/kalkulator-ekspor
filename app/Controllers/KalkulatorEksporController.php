32dc72b6d9633e31a7e5a2bee8b97ab1dac5761b
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-10-11 21:39:42 +0700

    controller of index cif, deleting comments, delete cfr, add cif, delete cif

--- app/Controllers/KalkulatorEksporController.php
+++ app/Controllers/KalkulatorEksporController.php
@@ -17,16 +17,19 @@ class KalkulatorEksporController extends BaseController
         $model_exwork = new Exwork();
         $model_fob = new FOB();
         $model_cfr = new CFR();
+        $model_cif = new CIF();
         $model_satuan = new Satuan();
 
         $exwork = $model_exwork->findAll();
         $fob = $model_fob->findAll();
         $cfr = $model_cfr->findAll();
+        $cif = $model_cif->findAll();
         $satuan = $model_satuan->findAll();
 
         $data['exwork'] = $exwork;
         $data['fob'] = $fob;
         $data['cfr'] = $cfr;
+        $data['cif'] = $cif;
         $data['satuan'] = $satuan;
 
         return view('try-backend/kalkulator_ekspor', $data);
@@ -56,51 +59,6 @@ class KalkulatorEksporController extends BaseController
             return redirect()->to('/')->with('error', 'Data satuan tidak ditemukan.');
         }
     }
-
-    // public function hitung_exwork()
-    // {
-    //     $model_exwork = new Exwork();
-
-    //     // Retrieve all exwork data
-    //     $exwork = $model_exwork->findAll();
-
-    //     // Get input from POST request
-    //     $jumlah_barang = $this->request->getPost('jumlahBarang');
-    //     $jumlah_barang = str_replace('.', '', $jumlah_barang);
-    //     $hpp = $this->request->getPost('hpp');
-    //     $hpp = str_replace('.', '', $hpp);
-    //     $keuntungan = $this->request->getPost('keuntungan');
-    //     $keuntungan = str_replace('.', '', $keuntungan);
-
-    //     // Initialize variables
-    //     $exwork_lainnya = 0;
-
-    //     if (empty($exwork)) {
-    //         // If there are no exwork records
-    //         $harga_exwork = number_format($hpp + $keuntungan, 0, ',', '.');
-    //     } else {
-    //         // If there are exwork records
-    //         $jb_hpp_keuntungan = ($hpp + $keuntungan) * $jumlah_barang;
-
-    //         // Loop through all exwork input data from POST
-    //         foreach ($this->request->getPost() as $key => $value) {
-    //             if (strpos($key, 'exwork_') === 0) {
-    //                 // Divide each exwork value by jumlah_barang
-    //                 $value = str_replace('.', '', $value);
-    //                 $exwork_lainnya += $value;
-    //             }
-    //         }
-
-    //         // Calculate final exwork price
-    //         $harga_exwork = 'Rp. ' . number_format(($jb_hpp_keuntungan + $exwork_lainnya) / $jumlah_barang, 0, ',', '.');
-    //     }
-
-    //     // Set flashdata for the calculated exwork price
-    //     session()->setFlashdata('harga_exwork', $harga_exwork);
-
-    //     // Redirect to the desired route with the flashdata
-    //     return redirect()->to('/');
-    // }
 
     public function add_exwork()
     {
@@ -205,5 +163,52 @@ class KalkulatorEksporController extends BaseController
         }
 
         return redirect()->to('/')->with('success', 'Komponen CFR berhasil ditambahkan!');
+    }
+
+    public function delete_cfr($id)
+    {
+        $model_cfr = new CFR();
+
+        $model_cfr->delete($id);
+
+        return redirect()->to('/');
+    }
+
+    public function add_cif()
+    {
+        // Validate input
+        $validation = \Config\Services::validation();
+        $validation->setRules([
+            'komponenCIF.*' => 'required',  // Ensure each component is required
+        ]);
+
+        if (!$this->validate($validation->getRules())) {
+            // If validation fails, redirect back with errors
+            return redirect()->back()->with('errors', $validation->getErrors())->withInput();
+        }
+
+        // Get the array of komponenCIF
+        $komponenCIFArray = $this->request->getPost('komponenCIF');
+
+        $model_cif = new CIF();
+
+        // Loop through the array and insert each komponenCIF into the database
+        foreach ($komponenCIFArray as $komponenCIF) {
+            $data = [
+                'komponen_cif' => esc($komponenCIF),  // Sanitize the input
+            ];
+            $model_cif->insert($data);
+        }
+
+        return redirect()->to('/')->with('success', 'Komponen CIF berhasil ditambahkan!');
+    }
+
+    public function delete_cif($id)
+    {
+        $model_cif = new CIF();
+
+        $model_cif->delete($id);
+
+        return redirect()->to('/');
     }
 }
