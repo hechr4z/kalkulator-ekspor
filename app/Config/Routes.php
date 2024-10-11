fab76a4b8708d404668a65ec8a20a215b19b594e
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-10-11 21:38:55 +0700

    routes of delete cfr, add cif, delete cif

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,3 +20,7 @@ $routes->post('/komponen-fob/add', 'KalkulatorEksporController::add_fob');
 $routes->get('/komponen-fob/delete/(:num)', 'KalkulatorEksporController::delete_fob/$1');
 
 $routes->post('/komponen-cfr/add', 'KalkulatorEksporController::add_cfr');
+$routes->get('/komponen-cfr/delete/(:num)', 'KalkulatorEksporController::delete_cfr/$1');
+
+$routes->post('/komponen-cif/add', 'KalkulatorEksporController::add_cif');
+$routes->get('/komponen-cif/delete/(:num)', 'KalkulatorEksporController::delete_cif/$1');
